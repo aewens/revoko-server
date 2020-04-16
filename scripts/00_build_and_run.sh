6f9c59d1ff7f422018f7ef083454b220a7ad7288
aewens <email@aewens.com>
2020-04-15 21:17:34 -0500

    Fixed bugs in 00 script and added documentation, now supports log and pid files

--- scripts/00_build_and_run.sh
+++ scripts/00_build_and_run.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-go build -o api.o api.go
-chmod +x api.o
-./api.o -config $1
+# $1 = config file
+# $2 = log file
+# $3 = pid file
+
+# Kill previous process
+if [ -f $3 ]; then
+    kill -9 $(cat $3)
+fi
+
+# Get scope of script
+DIR=`dirname "$0"`
+SRC="$DIR/.."
+
+# Build the project to an executable
+go build -o $SRC/api.o $SRC/api.go
+chmod +x $SRC/api.o
+
+# Run project using config file and logging output to file
+nohup $SRC/api.o -config $1 2>&1 >> $2 &>/dev/null &
+
+# Write PID to pid file
+echo $! > $3
+
+# Remove config file
+rm $1
